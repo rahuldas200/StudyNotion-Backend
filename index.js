d086017f1e504458166cc31793e4940e28bdc867
rahul <rdas53300@gmail.com>
2024-03-15 01:14:19 +0530

    update

--- index.js
+++ index.js
@@ -38,6 +38,14 @@ app.use(
 	})
 );
 
+router.get("/", (req, res) => {
+	res.setHeader("Access-Control-Allow-Origin", "*")
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Max-Age", "1800");
+	res.setHeader("Access-Control-Allow-Headers", "content-type");
+	res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+});
+
 // Connecting to cloudinary
 cloudinaryConnect();
 
