7919a774dd78e2bf0529e80a5f601c2681d71666
rahul <rdas53300@gmail.com>
2024-03-20 01:11:53 +0530

    cors error fixed

--- index.js
+++ index.js
@@ -26,7 +26,16 @@ database.connect();
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(
+	{
+		origin: 'http://localhost:3000',
+		methods:[GET,POST,PUT],
+		allowedHeaders: ['Content-Type', 'Authorization'],
+		credentials: true
+	}
+	
+));
+  
 
 app.use(
 	fileUpload({
