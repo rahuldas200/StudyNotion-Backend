85bc5ad7732d7a45b8352fec573affb40fba4c7b
rahul <rdas53300@gmail.com>
2024-03-20 01:14:39 +0530

    cros error fixed

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(cors(
 	{
 		origin: 'http://localhost:3000',
-		methods:[GET,POST,PUT],
+		methods: ['GET', 'POST', 'PUT'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
 		credentials: true
 	}
