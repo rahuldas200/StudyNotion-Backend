ce18fa151b2b60ba03f81b4aab34a2fe2f3a98ac
rahul <rdas53300@gmail.com>
2024-03-20 01:31:33 +0530

    fixed error

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-// Importing necessary modules and packages
 const express = require("express");
 const app = express();
-const router = express.Router()
-const userRoutes = require('./routes/User');
-const profileRoutes = require('./routes/Profile');
-const courseRoutes = require('./routes/Course');
-// const paymentRoutes = require("./routes/Payments");
+const router = express.Router();
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const courseRoutes = require("./routes/Course");
 const contactUsRoute = require("./routes/Contact");
 const database = require("./config/database");
 const cookieParser = require("cookie-parser");
@@ -13,7 +11,7 @@ const cors = require("cors");
 const { cloudinaryConnect } = require("./config/cloudinary");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 // Setting up port number
 const PORT = process.env.PORT || 8000;
@@ -23,41 +21,38 @@ dotenv.config();
 
 // Connecting to database
 database.connect();
- 
+
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(
-	{
-		origin: 'http://localhost:3000',
-		methods: ['GET', 'POST', 'PUT'],
-		allowedHeaders: ['Content-Type', 'Authorization'],
-		credentials: true
-	}
-	
-));
-  
-
 app.use(
-	fileUpload({
-		useTempFiles: true,
-		tempFileDir: "/tmp/",
-	})
+  cors({
+    origin: "*", // Allow requests from all origins
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allow specified HTTP methods
+    allowedHeaders: "*", // Allow all headers
+    credentials: true, // Allow credentials
+  })
 );
 
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
 // Connecting to cloudinary
 cloudinaryConnect();
 
+// Define proxy middleware
 const apiProxy = createProxyMiddleware("/api", {
-	target: "https://study-notion-backend-eight.vercel.app",
-	changeOrigin: true,
-	pathRewrite: {
-	  "^/api": "/api/v1",
-	},
-  });
- 
-  
+  target: "https://study-notion-backend-eight.vercel.app",
+  changeOrigin: true,
+  pathRewrite: {
+    "^/api": "/api/v1",
+  },
+});
+
 // Use proxy middleware
 app.use(apiProxy);
 
@@ -65,20 +60,19 @@ app.use(apiProxy);
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/course", courseRoutes);
-// app.use("/api/v1/payment", paymentRoutes);
 app.use("/api/v1/reach", contactUsRoute);
 
 // Testing the server
 app.get("/", (req, res) => {
-	return res.json({
-		success: true,
-		message: "Your server is up and running ...",
-	});
+  return res.json({
+    success: true,
+    message: "Your server is up and running ...",
+  });
 });
 
 // Listening to the server
 app.listen(PORT, () => {
-	console.log(`App is listening at ${PORT}`);
+  console.log(`App is listening at ${PORT}`);
 });
 
 // End of code.
